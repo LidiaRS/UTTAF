d18fe7b385e0818d8580e91a3a77a1aea59e5525
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-02-24 18:36:16 -0500

    ✨ Preparando ambiente.

---
+++ GPEEC.UTTAF/UTTAF.Desktop/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace UTTAF.Desktop.Views
+{
+    /// <summary>
+    /// Lógica interna para MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView() => InitializeComponent();
+    }
+}
