9e6c33b3fe01d932b7e40097323f3663eca0f9ca
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-03-19 01:28:37 -0500

    🔔 Adicionando dialog para inserir as informaçoes da sessao.

---
+++ GPEEC.UTTAF/UTTAF.Desktop/Views/DialogHost/InputNewSessionNameView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace UTTAF.Desktop.Views.DialogHost
+{
+    /// <summary>
+    /// Interação lógica para InputNewSessionNameView.xam
+    /// </summary>
+    public partial class InputNewSessionNameView : UserControl
+    {
+        public InputNewSessionNameView() => InitializeComponent();
+
+        private void CreateSession(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
--- GPEEC.UTTAF/UTTAF.Desktop/Views/StartView.xaml.cs
+++ GPEEC.UTTAF/UTTAF.Desktop/Views/StartView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using MaterialDesignThemes.Wpf;
+
+using System.Windows;
+using System.Windows.Controls;
 
 namespace UTTAF.Desktop.Views
 {
@@ -7,9 +10,19 @@ namespace UTTAF.Desktop.Views
     /// </summary>
     public partial class StartView : UserControl
     {
-        public StartView()
+        public StartView() => InitializeComponent();
+
+        private void CreateSession(object sender, RoutedEventArgs e)
         {
-            InitializeComponent();
+            MaterialDesignThemes.Wpf.DialogHost.Show(new DialogHost.InputNewSessionNameView(), "CreateSessionDH", CreateSessionClose);
+        }
+
+        private void CreateSessionClose(object sender, DialogClosingEventArgs e)
+        {
+            if ((bool)e.Parameter == true)
+            {
+                ButtonAux.Command.Execute(null);
+            }
         }
     }
 }
