3bb36628b534e5fc4118a9838ce8c8779af828dc
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-03-19 23:43:48 -0500

    🔃 Separando informaçoes da tela de principal para a tela de inicio.

---
+++ GPEEC.UTTAF/UTTAF.Mobile/UTTAF.Mobile/Views/StartView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UTTAF.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartView : ContentPage
+    {
+        public StartView() => InitializeComponent();
+    }
+}
