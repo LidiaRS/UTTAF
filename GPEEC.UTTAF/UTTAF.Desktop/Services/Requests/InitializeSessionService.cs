2a2757fffaacc483f03d6e9a4f3ced3f7fad233b
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-03-22 19:55:01 -0500

    ➕ Criando service para fazer a requisiçao de criaçao da sessao no desktop.

---
+++ GPEEC.UTTAF/UTTAF.Desktop/Services/Requests/InitializeSessionService.cs
@@ -0,0 +1,25 @@
+﻿using Dependencies.Services;
+
+using RestSharp;
+
+using System.Threading.Tasks;
+
+using UTTAF.Dependencies.Helpers;
+using UTTAF.Dependencies.Models;
+
+namespace UTTAF.Desktop.Services.Requests
+{
+    internal class InitializeSessionService
+    {
+        internal async static Task<IRestResponse> InitSessionTaskAsync(AuthSessionModel authSession)
+        {
+            return await new RequestService()
+            {
+                URL = DataHelper.Uri,
+                URN = "Session",
+                Method = Method.POST,
+                Body = authSession
+            }.ExecuteTaskAsync();
+        }
+    }
+}
