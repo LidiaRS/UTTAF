888017ff06c3e5ae6a6509c731346ad3c6e54a81
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-03-26 23:56:06 -0500

    ➕ Adicionando metodo para conversar de bitmap para bitmapImage, e implementando viewmodel da tela de configuraçao no desktop.

---
+++ GPEEC.UTTAF/UTTAF.Desktop/Converters/ImageTypeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace UTTAF.Desktop.Converters
+{
+    internal class ImageTypeConverter
+    {
+        internal static BitmapImage BitmapToBitmapImage(Bitmap src)
+        {
+            using var ms = new MemoryStream();
+            src.Save(ms, ImageFormat.Bmp);
+            var image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.EndInit();
+            return image;
+        }
+    }
+}
--- GPEEC.UTTAF/UTTAF.Desktop/ViewModels/ConfigureViewModel.cs
+++ GPEEC.UTTAF/UTTAF.Desktop/ViewModels/ConfigureViewModel.cs
@@ -1,8 +1,9 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 using QRCoder;
 
-using System.Drawing;
+using System.Windows.Media;
 
 using UTTAF.Dependencies.Helpers;
 
@@ -10,10 +11,23 @@ namespace UTTAF.Desktop.ViewModels
 {
     internal class ConfigureViewModel : ViewModelBase
     {
+        private string _reference;
+
+        public ImageSource QrCode { get; private set; }
+
+        public string Reference
+        {
+            get => _reference;
+            set => Set(ref _reference, value);
+        }
+
+        public RelayCommand CreateSessionCommand { get; private set; }
+
         public ConfigureViewModel() => Init();
 
         private void Init()
         {
+            Reference = DataHelper.AuthSession.SessionReference;
         }
 
         private void GenerateQrCode()
@@ -21,7 +35,7 @@ namespace UTTAF.Desktop.ViewModels
             using var generator = new QRCodeGenerator();
             using QRCodeData data = generator.CreateQrCode(DataHelper.AuthSession.SessionReference, QRCodeGenerator.ECCLevel.Q);
             using var qRCode = new QRCode(data);
-            qRCode.GetGraphic(10, Color.Black, Color.White, false);
+            qRCode.GetGraphic(10, System.Drawing.Color.Black, System.Drawing.Color.White, false);
         }
     }
 }
