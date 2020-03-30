bbc097aa30ea2b50e3a0b3434606cbc15792c5b1
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-03-29 23:04:14 -0500

    🦟 Corrigindo bug ao clicar no botar 'continuar' na tela de criaçao da sessao no desktop.

--- GPEEC.UTTAF/UTTAF.Desktop/Views/ConfigureView.xaml.cs
+++ GPEEC.UTTAF/UTTAF.Desktop/Views/ConfigureView.xaml.cs
@@ -1,9 +1,22 @@
-﻿using System.Windows;
+﻿using MaterialDesignThemes.Wpf.Transitions;
+
+using System.Windows;
+using System.Windows.Controls;
+
+using UTTAF.Desktop.ViewModels;
 
 namespace UTTAF.Desktop.Views
 {
     public partial class ConfigureView : Window
     {
         public ConfigureView() => InitializeComponent();
+
+        private void ClosingWindow(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var transitioner = ((Content as Grid).Children[2] as Grid).Children[0] as Transitioner;
+            var createSessionView = (transitioner.Items[1] as TransitionerSlide).Content as CreateSessionView;
+            if (transitioner.SelectedIndex == 1 || ((transitioner.Items[0] as TransitionerSlide).Content as StartView).NextCreateSession.Visibility == Visibility.Visible)
+                (createSessionView.DataContext as CreateSessionViewModel).CancelSessionCreationCommand.Execute(createSessionView);
+        }
     }
 }
--- GPEEC.UTTAF/UTTAF.Desktop/Views/CreateSessionView.xaml.cs
+++ GPEEC.UTTAF/UTTAF.Desktop/Views/CreateSessionView.xaml.cs
@@ -22,11 +22,14 @@ namespace UTTAF.Desktop.Views
 
         internal void CancelSession()
         {
-            var view = (((((Application.Current.MainWindow as ConfigureView).Content as Grid).Children[2] as Grid).Children[0] as Transitioner).Items[0] as TransitionerSlide).Content as StartView;
-            view.StartCreateSession.Visibility = Visibility.Visible;
-            view.NextCreateSession.Visibility = Visibility.Collapsed;
+            if (Application.Current.MainWindow is ConfigureView configure)
+            {
+                var view = ((((configure.Content as Grid).Children[2] as Grid).Children[0] as Transitioner).Items[0] as TransitionerSlide).Content as StartView;
+                view.StartCreateSession.Visibility = Visibility.Visible;
+                view.NextCreateSession.Visibility = Visibility.Collapsed;
 
-            Transitioner.MoveFirstCommand.Execute(null, null);
+                Transitioner.MoveFirstCommand.Execute(null, null);
+            }
         }
     }
 }
