6e94f85078ad380f44b2669203c599c61d8de30e
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-03-30 22:23:37 -0500

    🔃 Refatorando view de entrar na sessao no mobile.

--- GPEEC.UTTAF/UTTAF.Mobile/UTTAF.Mobile/Converters/Binding/JoinedMessageBindingConverter.cs
+++ GPEEC.UTTAF/UTTAF.Mobile/UTTAF.Mobile/Converters/Binding/JoinedMessageBindingConverter.cs
@@ -8,7 +8,7 @@ namespace UTTAF.Mobile.Converters.Binding
     internal class JoinedMessageBindingConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
-            $"Olá {(string)value}, A sessao irá começar em breve.";
+            $"Olá {(string)value}, a sessao irá começar em breve.";
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
