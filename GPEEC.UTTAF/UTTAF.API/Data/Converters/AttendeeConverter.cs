d46aba354db9d070fab5bc9df3f3a7c67d8fc4f7
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-15 22:43:53 -0500

    Criando converter dos participantes [API]

---
+++ GPEEC.UTTAF/UTTAF.API/Data/Converters/AttendeeConverter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using UTTAF.API.Data.Converters.Interfaces;
+using UTTAF.API.Models;
+using UTTAF.Dependencies.Data.VOs;
+
+namespace UTTAF.API.Data.Converters
+{
+	public class AttendeeConverter : IParser<AttendeeModel, AttendeeVO>, IParser<AttendeeVO, AttendeeModel>
+	{
+		public AttendeeModel Parse(AttendeeVO origin)
+		{
+			if (origin is null)
+				return null;
+
+			return new AttendeeModel
+			{
+				Id = origin.Id,
+				Name = origin.Name,
+				SessionReference = origin.SessionReference
+			};
+		}
+
+		public AttendeeVO Parse(AttendeeModel origin)
+		{
+			if (origin is null)
+				return null;
+
+			return new AttendeeVO
+			{
+				Id = origin.Id,
+				Name = origin.Name,
+				SessionReference = origin.SessionReference
+			};
+		}
+
+		public List<AttendeeModel> ParseList(List<AttendeeVO> origin)
+		{
+			return origin switch
+			{
+				null => null,
+				_ => origin.Select(x => Parse(x)).ToList()
+			};
+		}
+
+		public List<AttendeeVO> ParseList(List<AttendeeModel> origin)
+		{
+			return origin switch
+			{
+				null => null,
+				_ => origin.Select(x => Parse(x)).ToList()
+			};
+		}
+	}
+}
