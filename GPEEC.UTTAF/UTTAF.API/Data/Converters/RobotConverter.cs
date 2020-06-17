a10b3a543aced6f2f8250edeab370d6ff6b773e0
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-17 14:35:46 -0500

    Criando RobotVO e converter [API]

---
+++ GPEEC.UTTAF/UTTAF.API/Data/Converters/RobotConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UTTAF.API.Data.Converters.Interfaces;
+using UTTAF.API.Models;
+using UTTAF.Dependencies.Data.VOs;
+
+namespace UTTAF.API.Data.Converters
+{
+	public class RobotConverter : IParser<RobotModel, RobotVO>, IParser<RobotVO, RobotModel>
+	{
+		public RobotModel Parse(RobotVO origin)
+		{
+			if (origin is null)
+				return null;
+
+			return new RobotModel
+			{
+				DataOperation = origin.DataOperation,
+				RobotId = origin.RobotId,
+				RobotStatus = origin.RobotStatus,
+				SessionReference = origin.SessionReference
+			};
+		}
+
+		public RobotVO Parse(RobotModel origin)
+		{
+			if (origin is null)
+				return null;
+
+			return new RobotVO
+			{
+				RobotId = origin.RobotId,
+				SessionReference = origin.SessionReference,
+				RobotStatus = origin.RobotStatus,
+				DataOperation = origin.DataOperation
+			};
+		}
+	}
+}
---
+++ GPEEC.UTTAF/UTTAF.Dependencies/Data/VOs/RobotVO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using UTTAF.Dependencies.Enums;
+
+namespace UTTAF.Dependencies.Data.VOs
+{
+	public class RobotVO
+	{
+		public Guid RobotId { get; set; }
+
+		public string SessionReference { get; set; }
+
+		public RobotStatusEnum RobotStatus { get; set; }
+
+		public DateTime DataOperation { get; set; }
+	}
+}
