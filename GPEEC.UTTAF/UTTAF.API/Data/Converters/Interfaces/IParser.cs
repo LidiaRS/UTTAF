60c14e02b10a7a68dfc62d2a1bfe656a255f972c
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-17 14:35:23 -0500

    Criando IParserList<O, D> e removendo ParseList do IParser<O, D> [API]

--- GPEEC.UTTAF/UTTAF.API/Data/Converters/Interfaces/IParser.cs
+++ GPEEC.UTTAF/UTTAF.API/Data/Converters/Interfaces/IParser.cs
@@ -1,11 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace UTTAF.API.Data.Converters.Interfaces
+﻿namespace UTTAF.API.Data.Converters.Interfaces
 {
 	public interface IParser<TOrigin, TDestiny>
 	{
 		TDestiny Parse(TOrigin origin);
-
-		List<TDestiny> ParseList(List<TOrigin> origin);
 	}
 }
---
+++ GPEEC.UTTAF/UTTAF.API/Data/Converters/Interfaces/IParserList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace UTTAF.API.Data.Converters.Interfaces
+{
+	public interface IParserList<TOrigin, TDestiny>
+	{
+		IEnumerable<TDestiny> ParseList(IEnumerable<TOrigin> origin);
+	}
+}
