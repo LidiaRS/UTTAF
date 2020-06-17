fb63c435b973309440dd7ccb55061f872646a5e2
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-17 15:53:02 -0500

    Adicionando ISessionClient [Dependencies]

---
+++ GPEEC.UTTAF/UTTAF.Dependencies/Data/Clients/Interfaces/ISessionClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+using UTTAF.Dependencies.Data.VOs;
+
+namespace UTTAF.Dependencies.Data.Clients.Interfaces
+{
+	public interface ISessionClient
+	{
+		Task CreatedSessionAsync(AuthSessionVO createdSession);
+
+		Task NotCreatedSessionAsync();
+
+		Task AlreadyExistsSessionAsync();
+	}
+}
