b06365c8eabafcc31726f64dece3d24a93a1ac2c
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-23 23:57:24 -0500

    Removendo necessidade do ParseList no Converter dos participantes [API]

--- GPEEC.UTTAF/UTTAF.API/Data/Converters/AttendeeConverter.cs
+++ GPEEC.UTTAF/UTTAF.API/Data/Converters/AttendeeConverter.cs
@@ -1,13 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-using UTTAF.API.Data.Converters.Interfaces;
+﻿using UTTAF.API.Data.Converters.Interfaces;
 using UTTAF.API.Models;
 using UTTAF.Dependencies.Data.VOs;
 
 namespace UTTAF.API.Data.Converters
 {
-	public class AttendeeConverter : IParser<AttendeeModel, AttendeeVO>, IParser<AttendeeVO, AttendeeModel>, IParserList<AttendeeModel, AttendeeVO>, IParserList<AttendeeVO, AttendeeModel>
+	public class AttendeeConverter : IParser<AttendeeModel, AttendeeVO>, IParser<AttendeeVO, AttendeeModel>
 	{
 		public AttendeeModel Parse(AttendeeVO origin)
 		{
@@ -32,24 +29,6 @@ namespace UTTAF.API.Data.Converters
 				Id = origin.Id,
 				Name = origin.Name,
 				SessionReference = origin.SessionReference
-			};
-		}
-
-		public IEnumerable<AttendeeModel> ParseList(IEnumerable<AttendeeVO> origin)
-		{
-			return origin switch
-			{
-				null => null,
-				_ => origin.Select(x => Parse(x)).ToList()
-			};
-		}
-
-		public IEnumerable<AttendeeVO> ParseList(IEnumerable<AttendeeModel> origin)
-		{
-			return origin switch
-			{
-				null => null,
-				_ => origin.Select(x => Parse(x)).ToList()
 			};
 		}
 	}
