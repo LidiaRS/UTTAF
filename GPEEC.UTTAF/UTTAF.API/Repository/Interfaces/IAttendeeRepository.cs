653ca74da8f1248bf6d3d52109f0d3c23bc6a568
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-25 16:31:31 -0500

    Refatorando AttendeeRepository [API]

--- GPEEC.UTTAF/UTTAF.API/Repository/Interfaces/IAttendeeRepository.cs
+++ GPEEC.UTTAF/UTTAF.API/Repository/Interfaces/IAttendeeRepository.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 using UTTAF.API.Models;
 
 namespace UTTAF.API.Repository.Interfaces
 {
 	public interface IAttendeeRepository
-    {
-        Task<AttendeeModel> AddAttendeeTaskAsync(AttendeeModel attendee);
+	{
+		Task<AttendeeModel> AddAttendeeTaskAsync(AttendeeModel attendee);
 
-        Task<IEnumerable<AttendeeModel>> GetAttendersTaskAsync(string reference);
-
-        Task<bool> ClearAttendeersTaskAsync(SessionModel model);
-
-        Task<bool> LeaveAttendeeTaskAsync(AttendeeModel attendee);
-    }
+		Task LeaveAttendeeTaskAsync(AttendeeModel attendee);
+	}
 }
