2cdb8554d40464636068ebda8afe4b11c48eab70
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-25 23:54:36 -0500

    Implementando business do participante [API]

--- GPEEC.UTTAF/UTTAF.API/Business/AttendeeBusiness.cs
+++ GPEEC.UTTAF/UTTAF.API/Business/AttendeeBusiness.cs
@@ -1,34 +1,49 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 using UTTAF.API.Business.Interfaces;
 using UTTAF.API.Data.Converters;
+using UTTAF.API.Models;
+using UTTAF.API.Repository.Interfaces;
 using UTTAF.Dependencies.Data.VOs;
 
 namespace UTTAF.API.Business
 {
 	public class AttendeeBusiness : IAttendeeBusiness
 	{
+		private readonly IAttendeeRepository _attendeeRepository;
 		private readonly AttendeeConverter _attendeeConverter;
 
-		public AttendeeBusiness(AttendeeConverter attendeeConverter)
+		public AttendeeBusiness(AttendeeConverter attendeeConverter, IAttendeeRepository attendeeRepository)
 		{
+			_attendeeRepository = attendeeRepository;
 			_attendeeConverter = attendeeConverter;
 		}
 
-		public Task<AttendeeVO> JoinAtSessionTaskAsync(AttendeeVO newAttendee)
+		public async Task<AttendeeVO> JoinAtSessionTaskAsync(AttendeeVO newAttendee)
 		{
-			throw new NotImplementedException();
+			if (!(_attendeeConverter.Parse(newAttendee) is AttendeeModel newAttendeeModel))
+				return null;
+
+			if (!(await _attendeeRepository.AddAttendeeTaskAsync(newAttendeeModel) is AttendeeModel addedAttendeeModel))
+				return null;
+
+			return _attendeeConverter.Parse(addedAttendeeModel);
 		}
 
-		public Task<AttendeeVO> FindByNameTaskAsync(AttendeeVO attendee)
+		public async Task<AttendeeVO> FindByNameTaskAsync(AttendeeVO attendee)
 		{
-			throw new NotImplementedException();
+			if (!(_attendeeConverter.Parse(attendee) is AttendeeModel attendeeModel))
+				return null;
+
+			return _attendeeConverter.Parse(await _attendeeRepository.FindByNameTaskAsync(attendeeModel));
 		}
 
-		public Task LeaveAtSessionAsync(AttendeeVO attendee)
+		public async Task LeaveAtSessionAsync(AttendeeVO attendee)
 		{
-			throw new NotImplementedException();
+			if (!(_attendeeConverter.Parse(attendee) is AttendeeModel attendeeModel))
+				return;
+
+			await _attendeeRepository.LeaveAttendeeTaskAsync(attendeeModel);
 		}
 	}
 }
