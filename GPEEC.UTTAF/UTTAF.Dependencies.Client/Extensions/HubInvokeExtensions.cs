800fa83adefcb86efe533ed5ff032bf433c26fb4
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-07-05 23:14:23 -0500

    Criando metodo de extensao para nao precisar passar o nome do metodo [dep.clients]

---
+++ GPEEC.UTTAF/UTTAF.Dependencies.Client/Extensions/HubInvokeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace UTTAF.Dependencies.Clients.Extensions
+{
+	public static class HubInvokeExtensions
+	{
+		public static async Task InvokeAsync(this HubConnection connection, [CallerMemberName] string methodName = null)
+		{
+			await connection.InvokeAsync(methodName);
+		}
+
+		public static async Task InvokeAsync(this HubConnection connection, object arg, [CallerMemberName] string methodName = null)
+		{
+			await connection.InvokeAsync(methodName, arg);
+		}
+
+		public static async Task InvokeAsync(this HubConnection connection, object arg1, object arg2, [CallerMemberName] string methodName = null)
+		{
+			await connection.InvokeAsync(methodName, arg1, arg2);
+		}
+	}
+}
